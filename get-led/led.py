bf6cce4b6dfd873cd99cda2fbcdf8122ff0cd86d
Uzedzhi <afdimval@gmail.com>
2026-04-08 18:15:55 +0300

    added led program

---
+++ get-led/led.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+
+state = 0
+while True:
+    GPIO.output(26, state)
+    state = not state
+    time.sleep(0.5)
